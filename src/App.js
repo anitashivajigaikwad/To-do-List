9d728d9ec0d15cec28834c0c825516e9e1050277
anita <gaikwadanita2403@gmail.com>
2023-02-16 14:01:32 +0530

    to-do list

---
+++ src/App.js
@@ -0,0 +1,14 @@
+//import logo from './logo.svg';
+import './App.css';
+import './TodoList'
+import TodoList from './TodoList';
+
+function App() {
+  return (
+    <div className="App">
+      <TodoList></TodoList>
+    </div>
+  );
+}
+
+export default App;
